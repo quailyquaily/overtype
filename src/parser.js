78729193ebce62b09f63d713a91f9f926163781e
Lyric <5h3ll3x@gmail.com>
2025-10-27 23:59:53 +0900

    fix: escaped twice issue of links

--- src/parser.js
+++ src/parser.js
@@ -355,7 +355,7 @@ export class MarkdownParser {
         // URL should NOT be processed for markdown - use it as-is
         const anchorName = `--link-${this.linkIndex++}`;
         const safeUrl = this.sanitizeUrl(sanctuary.url);
-        replacement = `<a href="${safeUrl}" style="anchor-name: ${anchorName}"><span class="syntax-marker">[</span>${processedLinkText}<span class="syntax-marker url-part">](${this.escapeHtml(sanctuary.url)})</span></a>`;
+        replacement = `<a href="${safeUrl}" style="anchor-name: ${anchorName}"><span class="syntax-marker">[</span>${processedLinkText}<span class="syntax-marker url-part">](${sanctuary.url})</span></a>`;
       }
       
       html = html.replace(placeholder, replacement);
